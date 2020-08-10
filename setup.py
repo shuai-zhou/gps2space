87194163edc2824b360aa17e58a4c07ea0b12719
Iorent <zhsh0312@yeah.net>
2020-08-10 10:43:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='GPS2space',
-      version='0.1',
+setup(name='gps2space',
+      version='0.1.1',
       description='Toolbox for building spatial data and activity space from raw GPS data',
-      url='https://github.com/shuai-zhou/GPS2space',
+      url='https://github.com/shuai-zhou/gps2space',
       author='Shuai Zhou',
       author_email='sxz217@psu.edu',
       license='MIT',
-      packages=['GPS2space'],
+      packages=['gps2space'],
       include_package_data=True,
       install_requires=['pandas',
                                   'geopandas',
