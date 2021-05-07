3775a7896c51d43f45439aa0e157cc568bd12c7d
Shuai Zhou <iorent0312@yahoo.com>
2021-05-06 20:56:06 -0400

    update version to 0.2

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='gps2space',
-      version='0.1.1',
-      description='Toolbox for building spatial data and activity space from raw GPS data',
+      version='0.2',
+      description='Toolbox for building spatial data and activity space and shared space from raw GPS data',
       url='https://github.com/shuai-zhou/gps2space',
       author='Shuai Zhou',
       author_email='sxz217@psu.edu',
@@ -10,8 +10,8 @@ setup(name='gps2space',
       packages=['gps2space'],
       include_package_data=True,
       install_requires=['pandas',
-                                  'geopandas',
-                                  'numpy',
-                                  'scipy',
-                                  'shapely'],
+                        'geopandas',
+                        'numpy',
+                        'scipy',
+                        'shapely'],
       zip_safe=False)
