405a8949a357d1bcab7366b3f93557174050d5ed
Shuai Zhou <iorent0312@yahoo.com>
2021-06-14 23:46:13 -0400

    added dist2poly

--- gps2space/dist.py
+++ gps2space/dist.py
@@ -1,6 +1,6 @@
 """
 
-A module to measure distance
+A module to measure the nearest distance from point to point and polygon
 
 """
 import itertools
@@ -18,13 +18,13 @@ def dist_to_point(gpd_a, gpd_b, proj=2163):
 	==========
 	gpd_a: The GeoDataFrame you want to find their nearest points
 	gpd_b: The GeoDataFrame from where you are looking for nearest points
+	proj: Projection system
 
 	Returns
 	=======
 	gdf: GeoDataFrame (user-defined projection)
 	"""
-	# gpd_a.crs = "epsg:4326"
-	# gpd_b.crs = "epsg:4326"
+
 	gpd_a = gpd_a.to_crs(epsg=proj)
 	gpd_b = gpd_b.to_crs(epsg=proj)
 	n_a = np.array(list(zip(gpd_a.geometry.x, gpd_a.geometry.y)))
@@ -36,19 +36,66 @@ def dist_to_point(gpd_a, gpd_b, proj=2163):
                      pd.Series(dist, name='dist2point')], axis=1)
 	return gdf
 
-# For multi-line geometry, the following will raise an error:
-# Multi-part geometries do not provide a coordinate sequence
-# See: dist2line.ipynb
+def closest_poly(x, gdf_target, spatial_index, search_radius=None):
 
-# def dist_to_line(gpd_a, gpd_b, gpd_b_cols=['FULLNAME']):
-# 	a = np.concatenate([np.array(geom.coords) for geom in gpd_a.geometry.to_list()])
-# 	b = [np.array(geom.coords) for geom in gpd_b.geometry.to_list()]
-# 	b_ix = tuple(itertools.chain.from_iterable([itertools.repeat(i,x) for i, x in enumerate(list(map(len, b)))]))
-# 	b = np.concatenate(b)
-# 	ckd_tree = cKDTree(b)
-# 	dist, idx = ckd_tree.query(a, k=1)
-# 	idx = itemgetter(*idx)(b_ix)
-# 	gdf = pd.concat([gpd_a, gpd_b.loc[idx, gpd_b_cols].reset_index(drop=True), pd.Series(dist, name='dist2line')], axis=1)
-# 	return gdf
+	"""
+	Detect the nearest polygons based on SEARCH_RADIUS (buffer size)
 
+	Parameters
+	==========
+	gdf_target: The GeoDataFrame from which you want to find the nearest polygons
+	spatial_index: Spatial index of the gpd_target GeoPandas dataframe
+	search_radius: Search radius, i.e., the buffer size to buffer point features
+
+	Returns
+	=======
+	xxx: xxx
+	"""
+
+    # SEARCH_RADIUS in meters
+	search_radius = search_radius
+	bbox = x.buffer(search_radius, cap_style=3)
+	possible_matches_index = list(spatial_index.intersection(bbox.bounds))
+
+    # If no intersection, return NaN
+	if len(possible_matches_index) == 0:
+		return None
+
+    # If has intersections, return the minimum distance
+	else:
+		possible_matches = gdf_target.iloc[possible_matches_index]
+		return possible_matches.distance(x).min()
+
+def dist_to_poly(gdf_source, gdf_target, proj=2163, search_radius=None):
+	"""
+	Perform the nearest distance measures from a source GeoPandas dataframe to a target GeoPandas dataframe
+
+	Parameters
+	==========
+	gdf_souruce: The source GeoDataFrame contains point features
+	gpd_target: The GeoDataFrame from which you want to find the nearest polygons from the gdf_source
+	proj: Projection system
+	search_radius: Search radius, i.e., the buffer size to buffer point features
+
+	Returns
+	=======
+	gdf: GeoDataFrame
+	"""
+    # Project GeoPandas dataframe and copy gdf_source
+	gdf_source = gdf_source.to_crs(epsg=proj)
+	gdf_target = gdf_target.to_crs(epsg=proj)
+	gdf_dist = gdf_source.copy()
+
+    # If SEARCH_RADIUS is not specified, iterate over all the features
+	if not search_radius:
+		gdf_dist['dist2poly'] = gdf_source.geometry.apply(
+                                lambda x: gdf_target.distance(x).min())
+
+    # If SEARCH_RADIUS is specified, keep those only within SEARCH_RADIUS, otherwise, make NaN
+	else:
+		spatial_index = gdf_target.sindex
+		gdf_dist['dist2poly'] = gdf_source.geometry.apply(
+                                lambda x: closest_poly(x, gdf_target, spatial_index, search_radius))
+
+	return gdf_dist
 
